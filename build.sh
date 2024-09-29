4a4c559d47d155d5cca66eeb5ea84cf7a070ba47
rwrq22 <61651734+rwrq22@users.noreply.github.com>
2024-09-29 19:28:24 +0900

    requirments.txt

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@ set -o errexit
 pip install --upgrade pip
 
 poetry install
+pip install -r requirements.txt
 pip install --force-reinstall -U setuptools
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
