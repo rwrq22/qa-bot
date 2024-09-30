f796a85b32f54b38665c160597d4163ca5edb97d
rwrq22 <61651734+rwrq22@users.noreply.github.com>
2024-09-30 12:26:12 +0900

    settings.py cors,csrf

--- config/settings.py
+++ config/settings.py
@@ -161,8 +161,8 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
     CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 else:
-    CORS_ALLOWED_ORIGINS = ["https://qabotjs.xyz"]
-    CSRF_TRUSTED_ORIGINS = ["https://qabotjs.xyz"]
+    CORS_ALLOWED_ORIGINS = ["https://qabotjs.xyz", "https://www.qabotjs.xyz"]
+    CSRF_TRUSTED_ORIGINS = ["https://qabotjs.xyz", "https://www.qabotjs.xyz"]
 
 CORS_ALLOW_CREDENTIALS = True
 
