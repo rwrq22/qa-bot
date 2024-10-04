238b9e1af2c9a1ccd1d463ed783b7de21fc4fc10
rwrq22 <61651734+rwrq22@users.noreply.github.com>
2024-10-04 19:09:33 +0900

    언어모델 분리

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -o errexit
 pip install --upgrade pip
 
 poetry install
-pip install -r requirements.txt
 pip install --force-reinstall -U setuptools
 # Convert static asset files
 python manage.py collectstatic --no-input
