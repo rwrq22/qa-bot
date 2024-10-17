a1f13d0b2867c342e7e3fca8fec827e1fa6de3d2
rwrq22 <61651734+rwrq22@users.noreply.github.com>
2024-10-17 16:54:10 +0900

    ec2 코드로 수정

--- chat_messages/models.py
+++ chat_messages/models.py
@@ -2,9 +2,8 @@
 from django.db import models
 
 # Third party packages
-import json, environ, os
+import environ, os, requests
 from pathlib import Path
-import boto3
 
 # Django applications
 from common.models import CommonModel
@@ -15,25 +14,14 @@ env = environ.Env()
 if DEBUG:
     BASE_DIR = Path(__file__).resolve().parent.parent
     environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_DEFAULT_REGION = env("AWS_DEFAULT_REGION")
-AWS_ENDPOINT_NAME = env("AWS_ENDPOINT_NAME")
+A_URL = env("A_URL")
 
 
 def get_model_answer(question, n):
     try:
-        client = boto3.client(
-            "sagemaker-runtime",
-            region_name="ap-northeast-2",
-        )
-        response = client.invoke_endpoint(
-            EndpointName=AWS_ENDPOINT_NAME,
-            ContentType="application/json",
-            Accept="application/json",
-            Body=(json.dumps({"question": question, "n": n})),
-        )
-        result = json.loads(response["Body"].read())
+        data = {"question": question, "n": n}
+        response = requests.post(A_URL, json=data)
+        result = response.json()
         if result["answer"] == "-1":
             return "잘 모르겠습니다."
         else:
